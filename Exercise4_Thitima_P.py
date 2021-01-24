a72998c2ac6211edb23ba712568dc4ecc787a9dd
medsafety <rxtu2019krabihealthoffice@gmail.com>
2021-01-24 17:41:14 +0700

    Create Exercise4_Thitima_P.py
    Exercise 4 Homework

---
+++ Exercise4_Thitima_P.py
@@ -0,0 +1,10 @@
+print ('โปรแกรมการเก็บคะแนนรายบุคคลของนักศึกษาชั้นปีที่ 2 เทอม 1')
+print('--- Your Score ---')
+Score=60
+print('Foundation English : ',Score)
+Score=80
+print('General Business : ',Score)
+Score=90
+print('Introduction to Computer Systems : ',Score)
+Score=40
+print('Computer Programming : ',Score)
