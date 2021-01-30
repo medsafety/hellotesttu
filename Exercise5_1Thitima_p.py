45d720aad796756c3bc4f9319eb8c8e0b3263bf6
medsafety <rxtu2019krabihealthoffice@gmail.com>
2021-01-30 17:22:17 +0700

    homeworkno5

---
+++ Exercise5_1Thitima_p.py
@@ -0,0 +1,11 @@
+print("โปรแกรมบวกลบคูณหารเลขสองตัว")
+num1= int(input("กรอกเลขตัวที่1 : "))
+num2 = int(input("กรอกเลขตัวที่2 : "))
+result_puls=num1+num2
+result_minus=num1-num2
+result_multi=num1*num2
+result_divide=num1/num2
+print("ผลบวก",num1,"+",num2,"=",result_puls)
+print("ผลลบ",num1,"-",num2,"=",result_minus)
+print("ผลคูณ",num1,"x",num2,"=",result_multi)
+print("ผลหาร",num1,"/",num2,"=",result_divide)
